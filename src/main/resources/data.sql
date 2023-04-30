86ca09191127e5f162a8c6f2c0598af14163c165
ZeljanaGrujic <zg20180045@gmail.com>
2023-04-30 15:11:57 +0200

    Data.sql file changed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,5 +2,4 @@
 (6,'','kl2@gmail.com','Klijent 2 Prezime 2',4,'061-0001-110'),
 (1,'IZMENA klijenta','client1@gmail.com','Klijent 1 Prezime 1',2,'060-0000-011');
 */
-INSERT INTO USER VALUES(2,'test2','test2','test2','test2')
-INSERT INTO USER VALUES(3,'test23','test23','test23','test23')
+INSERT INTO USER VALUES(2,'test2','test2','test2','test2'),(3,'test23','test23','test23','test23')
