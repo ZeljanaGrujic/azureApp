2f0a9fa22ee68330ffd42d674fd3d9547ade14fb
ZeljanaGrujic <zg20180045@gmail.com>
2023-04-30 14:28:39 +0200

    Clean app

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+/*INSERT  INTO CLIENT (id,details,email,full_name,locationid,phone) values 
+(6,'','kl2@gmail.com','Klijent 2 Prezime 2',4,'061-0001-110'),
+(1,'IZMENA klijenta','client1@gmail.com','Klijent 1 Prezime 1',2,'060-0000-011');
+*/
+INSERT INTO USER VALUES(2,'test2','test2','test2','test2')
